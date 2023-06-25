72a29e99daa20921d96e54ce4cc1c1c360447fe1
kira.dob <arik.dobrynina@yandex.ru>
2023-06-25 16:47:55 +0700

    added Homework1 Main

---
+++ Homework1/Main.java
@@ -0,0 +1,32 @@
+package Homework1;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        1. Создать наследника реализованного класса ГорячийНапиток с дополнительным полем int температура.
+
+        // Product -> HotDrink -> HotTea, HotCoffee, HotChocolate
+
+//        2. Создать класс ГорячихНапитковАвтомат реализующий интерфейс ТорговыйАвтомат
+//          и реализовать перегруженный метод getProduct(int name, int volume, int temperature),
+//          выдающий продукт соответствующий имени, объёму и температуре
+
+//        3. В main проинициализировать несколько ГорячихНапитков и ГорячихНапитковАвтомат
+//          и воспроизвести логику, заложенную в программе
+
+//        4. Всё вышеуказанное создать согласно принципам ООП, пройденным на семинаре
+
+        HotDrink hotTeaSmall = new HotTea("Earl Grey", 2, 180, 80);
+        HotDrink hotCoffeeMedium = new HotCoffee("Lavazza", 3, 250, 75);
+        HotDrink hotChocolateLarge = new HotChocolate("DeMarco", 4, 380, 70);
+
+        VendingMachine hotDrinksVending = new HotDrinksVendingMachine();
+
+        hotDrinksVending.addProduct(hotTeaSmall);
+        hotDrinksVending.addProduct(hotCoffeeMedium);
+        hotDrinksVending.addProduct(hotChocolateLarge);
+
+        System.out.println(hotDrinksVending.getProduct("Earl Grey"));
+        System.out.println(hotDrinksVending.getProduct("DeMarco", 380, 70));
+    }
+}
