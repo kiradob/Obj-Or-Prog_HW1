86a21cd561fc2d0c626fb4447c3f9c88286be279
kira.dob <arik.dobrynina@yandex.ru>
2023-06-25 16:47:30 +0700

    added Homework1 HotTea

---
+++ Homework1/HotTea.java
@@ -0,0 +1,7 @@
+package Homework1;
+
+public class HotTea extends HotDrink {
+    public HotTea(String name, double price, int volume, int temp) {
+        super(name, price, volume, temp);
+    }
+}
