153a0a37ad82a1c022ee216b7274c9572435ac3a
kira.dob <arik.dobrynina@yandex.ru>
2023-06-25 16:46:02 +0700

    added Homework1 HotCoffee

---
+++ Homework1/HotCoffee.java
@@ -0,0 +1,7 @@
+package Homework1;
+
+public class HotCoffee extends HotDrink {
+    public HotCoffee(String name, double price, int volume, int temp) {
+        super(name, price, volume, temp);
+    }
+}
